d8a773d01c95e770f81cf7310a464f3f632b371f
Raunaq Rakesh <164249000+Raunaq-nous@users.noreply.github.com>
2026-04-28 20:51:33 +0530

    fix: import Link in ingest page

--- app/ingest/page.tsx
+++ app/ingest/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { Header, Footer } from "../components";
 import { saveApplication, generateSlug, generateId, TargetBucket } from "../../lib/store";
 import { scoreJob } from "../../lib/scoring";
